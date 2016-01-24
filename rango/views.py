6b29f8d330ed6f59b8152e975f57d1c5e4a55fe7
susahe <sumudu.susahe@gmail.com>
2016-01-24 03:49:03 +0100

    temaplate

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from django.http import HttpResponse
 
-# Create your views here.
+def oldindex(request):
+    return HttpResponse("Rango Sasy hey there world <br/> <a href='/rango/about'>About </a>")
+
+def oldabout(request):
+    return HttpResponse("Rango Say: Hellworld <br/> <a href='/rango/'> Home Page </a> ")
+
+def oldcontact(request):
+    return HttpResponse("<a href='/rango/'> About </a> " )
+
+def index (request):
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    return render (request, 'rango/index.html', context_dict )
+
+def about (request):
+    context_dict = {'boldmessage': " I am bold font about page hi"}
+    return render (request, 'rango/index.html', context_dict)
+
+def contact(request):
+    context_dict = {'boldmessage': " I am contact page hi "}
+    return render (request, 'rango/index.html', context_dict)
